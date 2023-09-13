86a2e720d7e0f7f69fbda10823219e35fa14bb00
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-09-13 18:18:53 +0100

    hir

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 const Hero = () => {
   return (
     <div className="relative">
-      <img src="/Group 71.svg" alt="Hero image" className="w-full h-auto" />
+      <img src="/Group 71.svg" alt="HeroImage" className="w-full h-auto" />
       <div className="absolute top-0 left-0 w-full hidden lg:block">
         <Navbar />
       </div>
